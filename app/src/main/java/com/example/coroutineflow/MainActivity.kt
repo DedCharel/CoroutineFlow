2d279a9622d02f883c9d11573e974960b2621679
Daniil Shcherbakov <Charel@bk.ru>
2024-11-21 19:59:08 +0300

    Create CoroutineFlow task

--- app/src/main/java/com/example/coroutineflow/MainActivity.kt
+++ app/src/main/java/com/example/coroutineflow/MainActivity.kt
@@ -5,8 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.coroutineflow.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
