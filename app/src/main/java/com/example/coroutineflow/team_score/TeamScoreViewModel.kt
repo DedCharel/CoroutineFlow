56f62c7c93d1f590e3ee74c8c63deda6a096ab1b
Daniil Shcherbakov <Charel@bk.ru>
2024-11-28 18:33:16 +0300

    Use SharedFlow for TeamScore

--- app/src/main/java/com/example/coroutineflow/team_score/TeamScoreViewModel.kt
+++ app/src/main/java/com/example/coroutineflow/team_score/TeamScoreViewModel.kt
@@ -1,41 +1,55 @@
 package com.example.coroutineflow.team_score
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
-class TeamScoreViewModel: ViewModel() {
+class TeamScoreViewModel : ViewModel() {
 
-    private val _state = MutableLiveData<TeamScoreState>(TeamScoreState.Game(0, 0))
-    val state: LiveData<TeamScoreState> = _state
+    private var cashedState: TeamScoreState = TeamScoreState.Game(0, 0)
 
-    fun increaseScore(team: Team){
-        val currentState = _state.value
-        if (currentState is TeamScoreState.Game) {
-            if (team == Team.Team1) {
-                val oldValue = currentState.score1
-                val newValue = oldValue + 1
-                _state.value = currentState.copy(score1 = newValue)
-                if (newValue >= WINNER_SCORE) {
-                    _state.value = TeamScoreState.Winner(
-                        winnerTeam = Team.Team1,
-                        score1 = newValue,
-                        score2 = currentState.score2
-                    )
-                }
-            } else {
-                val oldValue = currentState.score2
-                val newValue = oldValue + 1
-                _state.value = currentState.copy(score2 = newValue)
-                if (newValue >= WINNER_SCORE) {
-                    _state.value = TeamScoreState.Winner(
-                        winnerTeam = Team.Team2,
-                        score1 = currentState.score1,
-                        score2 = newValue
-                    )
+    private val _state = MutableSharedFlow<TeamScoreState>(replay = 1)
+    val state = _state.asSharedFlow()
+        .onEach { cashedState = it }
+
+
+    fun increaseScore(team: Team) {
+        viewModelScope.launch {
+            val currentState = cashedState
+            if (currentState is TeamScoreState.Game) {
+                if (team == Team.Team1) {
+                    val oldValue = currentState.score1
+                    val newValue = oldValue + 1
+                    _state.emit(currentState.copy(score1 = newValue))
+                    if (newValue >= WINNER_SCORE) {
+                        _state.emit(
+                            TeamScoreState.Winner(
+                                winnerTeam = Team.Team1,
+                                score1 = newValue,
+                                score2 = currentState.score2
+                            )
+                        )
+                    }
+                } else {
+                    val oldValue = currentState.score2
+                    val newValue = oldValue + 1
+                    _state.emit(currentState.copy(score2 = newValue))
+                    if (newValue >= WINNER_SCORE) {
+                        _state.emit(
+                            TeamScoreState.Winner(
+                                winnerTeam = Team.Team2,
+                                score1 = currentState.score1,
+                                score2 = newValue
+                            )
+                        )
+                    }
                 }
             }
         }
+
     }
 
     companion object {
